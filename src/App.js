4c37be8a4dcf8dc58867571ab2fa0b8c1c6951dd
TommasoBertocchiDeveloper <tommasobertocchideveloper04@gmail.com>
2023-12-17 17:10:24 +0100

    *

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+
+const articles = [
+  // Questi dati sono solo per l'esempio. Dovresti caricarli da un file JSON.
+  { id: 1, path: '1.jpg', title: 'Vietnamese Pho', excerpt: 'Discover the richness of Pho...' },
+  { id: 2, path: './2.jpg', title: 'Banh Mi', excerpt: 'The classic Vietnamese sandwich...' },
+  // Aggiungi altri articoli qui...
+];
+
+
+
+
+const HomePage = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container mx-auto px-4">
+      <header className="text-center my-12">
+        <h1 className="text-4xl font-bold text-gray-800">Flavors of Vietnam</h1>
+        <p className="text-gray-600">Explore the rich and diverse cuisine of Vietnam</p>
       </header>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {articles.map(article => (
+          <article key={article.id} className="bg-white rounded-lg shadow-lg overflow-hidden">
+            <img src={`${process.env.PUBLIC_URL}/img/${article.id}.jpg`} alt={article.title} className="w-full h-64 object-cover"/>
+            <div className="p-6">
+              <h2 className="font-bold text-2xl mb-2 text-gray-800">{article.title}</h2>
+              <p className="text-gray-600">{article.excerpt}</p>
+              {/* Aggiungi qui un link o un bottone per leggere l'articolo completo */}
+            </div>
+          </article>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default HomePage;
